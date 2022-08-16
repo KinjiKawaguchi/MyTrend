763028d2abb6118c37ac778e978729290c4c24de
KinjiKawaguchi <kawakin0310@icloud.com>
2022-08-16 09:23:02 +0900

    CSVファイル処理

--- src/bot.py
+++ src/bot.py
@@ -1,6 +1,8 @@
-import sys
+import csv
 import tweepy
 import os
+import csv
+import datetime
 
 CK = os.environ.get('API_KEY')
 CS = os.environ.get('API_KEY_SECRET')
@@ -11,6 +13,46 @@ auth = tweepy.OAuthHandler(CK, CS)
 auth.set_access_token(AT, AS)
 api = tweepy.API(auth)
 
-timeline = api.home_timeline()
-print(timeline)
-#api.update_status("私の名前はMyTrend。よろしくお願いします。")
+class mkcsv:
+    def __init__(self,filename):
+        self.finename = filename
+        with open(self.filename,mode='w',encoding="utf-8") as file:
+            writer = csv.writer(file)
+            header=[
+                "RT",
+                "text",
+                "tweet_id",
+                "post_date",
+                "retweet",
+                "favorite",
+                "user",
+                "screen_name",
+                "reply_id",
+                "language"
+            ]
+            writer.writerow(header)
+            
+    def mk(self,tweet):
+        with open(self.filename,mode='a',encoding="utf-8") as file:
+            writer = csv.writer(file)
+            if 'RT' in tweet.text:
+                RT = True
+            else:
+                RT = False
+            text = str(tweet.text).replace('\n','')
+            if text.find(','):
+                text.replace(',','，')
+
+            body=[
+                RT,
+                text,
+                tweet.id,
+                tweet.created_at + datetime.timedelta(hours=+9),
+                tweet.retweet_count,
+                tweet.favorite_count,
+                tweet.user.name,
+                tweet.user.screen_name,
+                tweet.in_reply_to_status_id,
+                tweet.lang
+            ]
+            writer.writerow(body)
